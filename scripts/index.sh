bd523aa64819a7049b701a3dbb9a7fcaf0f5e425
María Moreno <mariamorenog9@gmail.com>
2024-01-29 19:49:30 +0100

    First try w index script

--- scripts/index.sh
+++ scripts/index.sh
@@ -1,6 +1,15 @@
 # This script should index the genome file specified in the first argument ($1),
 # creating the index in a directory specified by the second argument ($2).
 
+genomefile="$1"
+
+echo "Indexing, please wait."
+
+indexdir="$2"
+
+STAR --runThreadN 4 --runMode genomeGenerate --genomeDir {$indexdir} \
+--genomeFastaFiles {$genomefile} --genomeSAindexNbases 9
+
 # The STAR command is provided for you. You should replace the parts surrounded
 # by "<>" and uncomment it.
 
