02037d4ba4e47140425264f667bcc922ad8c6f74
María Moreno <mariamorenog9@gmail.com>
2024-02-08 20:23:32 +0100

    Index and merge scripts

--- scripts/index.sh
+++ scripts/index.sh
@@ -7,11 +7,4 @@ echo "Indexing, please wait."
 
 indexdir="$2"
 
-STAR --runThreadN 4 --runMode genomeGenerate --genomeDir {$indexdir} \
---genomeFastaFiles {$genomefile} --genomeSAindexNbases 9
-
-# The STAR command is provided for you. You should replace the parts surrounded
-# by "<>" and uncomment it.
-
-# STAR --runThreadN 4 --runMode genomeGenerate --genomeDir <outdir> \
-# --genomeFastaFiles <genomefile> --genomeSAindexNbases 9
+STAR --runThreadN 4 --runMode genomeGenerate --genomeDir "$indexdir" --genomeFastaFiles "$genomefile" --genomeSAindexNbases 9
--- scripts/merge_fastqs.sh
+++ scripts/merge_fastqs.sh
@@ -3,3 +3,17 @@
 # stored in the output directory specified by the second argument ($2).
 #
 # The directory containing the samples is indicated by the first argument ($1).
+input_dir="$1"
+output_dir="$2"
+sample_id="$3"
+
+output_file="${output_dir}/${sample_id}.fastq.gz"
+
+echo: "Ficheros a procesar"
+ls -l "${input_dir}/${sample_id}.5dpp.1.1s_sRNA.fastq" "${input_dir}/${sample_id}.5dpp.1.2s_sRNA.fastq"
+
+echo "Ficheros listos"
+echo "${output_file}"
+
+cat "${input_dir}/${sample_id}.5dpp.1.1s_sRNA.fastq" "${input_dir}/${sample_id}.5dpp.1.2s_sRNA.fastq" > "${output_file}"
+echo "Files merged into ${output_file}"
